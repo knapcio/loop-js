a51ef3a53cdaf796835f392afb75fac3d6d867b4
knapcio <knapcio@gmail.com>
2018-06-24 05:59:50 +0200

    loops added

---
+++ index.js
@@ -0,0 +1,24 @@
+export const loop = function(array, callback)
+{
+  array.forEach(callback);
+}
+
+export const loopBackwards = function(array, callback)
+{
+  // ...
+}
+
+export const loopEven = function(array, callback)
+{
+  // ...
+}
+
+export const loopOdd = function(array, callback)
+{
+  // ...
+}
+
+export const loopWhatever = function(array, callback)
+{
+  // ...
+}
